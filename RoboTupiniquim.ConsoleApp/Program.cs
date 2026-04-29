b111b6b203504d4acf36925324aab2cb37dcb12a
Felipe Neto Mello <fnm03br@gmail.com>
2026-04-28 21:57:28 -0300

    Refatorado com métodos dentro da mesma classe

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -1,90 +1,119 @@
 ﻿//PROJETO ROBÔ TUPINIQUIM
 
-//1. Inicializar posições iniciais e orientação
-//Console.Clear();
-Console.Write("Digite a posição inicial do robô no eixo X: ");
-int posicaoInicialEixoX = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("\nDigite a posição inicial do robô no eixo Y: ");
-int posicaoInicialEixoY = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("\nDigite a orientação inicial (N, S, L ou O): ");
-string? orientacao = Console.ReadLine()?.ToUpper();
-
-Console.WriteLine($"\nPosição Inicial: {posicaoInicialEixoX} {posicaoInicialEixoY} {orientacao}");
-
-//2. Receber comando
-Console.Write("\nDigite uma sequência de instruções seguindo as regras: " +
-"\nE - virar robô para Esquerda." +
-"\nD - virar robô para Direita." +
-"\nM - mover (andar)." +
-"\nComando: ");
-string comando = Console.ReadLine().ToUpper();
-Console.WriteLine($"\nInstruções: {comando}");
-
-//3. Quebrar comando em instruções
-char[] instrucoes = comando.ToCharArray();
-
-//4. Verificar instrução
-for (int i = 0; i < instrucoes.Length; i++)
+class Program
 {
-    //verificando direção se instrução D
-    if (instrucoes[i] == 'D' && orientacao == "N")
+    static void Main(string[] args)
     {
-        orientacao = "O";
-    }
-    else if (instrucoes[i] == 'D' && orientacao == "S")
-    {
-        orientacao = "L";
-    }
-    else if (instrucoes[i] == 'D' && orientacao == "O")
-    {
-        orientacao = "S";
-    }
-    else if (instrucoes[i] == 'D' && orientacao == "L")
-    {
-        orientacao = "N";
+        //1. Inicializar posições iniciais e orientação
+        //Console.Clear();
+        Console.Write("Digite a posição inicial do robô no eixo X: ");
+        int posicaoInicialEixoX = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("\nDigite a posição inicial do robô no eixo Y: ");
+        int posicaoInicialEixoY = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("\nDigite a orientação inicial (N, S, L ou O): ");
+        string orientacao = Console.ReadLine().ToUpper();
+
+        Console.WriteLine($"\nPosição Inicial: {posicaoInicialEixoX} {posicaoInicialEixoY} {orientacao}");
+
+        //2. Receber comando
+        Console.Write("\nDigite uma sequência de instruções seguindo as regras: " +
+        "\nE - virar robô para Esquerda." +
+        "\nD - virar robô para Direita." +
+        "\nM - mover (andar)." +
+        "\nComando: ");
+        string comando = Console.ReadLine().ToUpper();
+        Console.WriteLine($"\nInstruções: {comando}");
+
+        //3. Quebrar comando em instruções
+        char[] instrucoes = comando.ToCharArray();
+
+        //4. Verificar instrução
+        for (int i = 0; i < instrucoes.Length; i++)
+        {
+            //verificando direção se instrução D
+            if (instrucoes[i] == 'D')
+                ComandoADireita(instrucoes[i], orientacao);
+
+            //verificando direção se instrução E
+            else if (instrucoes[i] == 'E')
+                ComandoAEsquerda(instrucoes[i], orientacao);
+
+            //verificando movimento
+            else
+                ComandoMovimento(instrucoes[i], orientacao, posicaoInicialEixoX, posicaoInicialEixoY);
+        }
+
+        Console.WriteLine($"\nPosição Final: {posicaoInicialEixoX} {posicaoInicialEixoY} {orientacao}");
+        Console.ReadLine();
     }
 
-    //verificando direção se instrução E
-    if (instrucoes[i] == 'E' && orientacao == "N")
+    static string ComandoADireita(char instrucao, string orientacao)
     {
-        orientacao = "L";
-    }
-    else if (instrucoes[i] == 'E' && orientacao == "S")
-    {
-        orientacao = "O";
-    }
-    else if (instrucoes[i] == 'E' && orientacao == "O")
-    {
-        orientacao = "N";
-    }
-    else if (instrucoes[i] == 'E' && orientacao == "L")
-    {
-        orientacao = "S";
+
+        if (instrucao == 'D' && orientacao == "N")
+        {
+            orientacao = "O";
+        }
+        else if (instrucao == 'D' && orientacao == "S")
+        {
+            orientacao = "L";
+        }
+        else if (instrucao == 'D' && orientacao == "O")
+        {
+            orientacao = "S";
+        }
+        else if (instrucao == 'D' && orientacao == "L")
+        {
+            orientacao = "N";
+        }
+
+        return orientacao;
     }
 
-    //verificando movimento
-    if (instrucoes[i] == 'M' && orientacao == "N")
+    static string ComandoAEsquerda(char instrucao, string orientacao)
     {
-        posicaoInicialEixoY = posicaoInicialEixoY + 1;
-    }
-    else if (instrucoes[i] == 'M' && orientacao == "S")
-    {
-        posicaoInicialEixoY = posicaoInicialEixoY - 1;
-    }
-    else if (instrucoes[i] == 'M' && orientacao == "O")
-    {
-        posicaoInicialEixoX = posicaoInicialEixoX + 1;
-    }
-    else if (instrucoes[i] == 'M' && orientacao == "L")
-    {
-        posicaoInicialEixoX = posicaoInicialEixoX - 1;
+        if (instrucao == 'E' && orientacao == "N")
+        {
+            orientacao = "L";
+        }
+        else if (instrucao == 'E' && orientacao == "S")
+        {
+            orientacao = "O";
+        }
+        else if (instrucao == 'E' && orientacao == "O")
+        {
+            orientacao = "N";
+        }
+        else if (instrucao == 'E' && orientacao == "L")
+        {
+            orientacao = "S";
+        }
+        return orientacao;
     }
 
+    static int ComandoMovimento(char instrucao, string orientacao, int posicaoInicialEixoX, int posicaoInicialEixoY)
+    {
+        if (instrucao == 'M' && orientacao == "N")
+        {
+            posicaoInicialEixoY = posicaoInicialEixoY + 1;
+            return posicaoInicialEixoY;
+        }
+        else if (instrucao == 'M' && orientacao == "S")
+        {
+            posicaoInicialEixoY = posicaoInicialEixoY - 1;
+            return posicaoInicialEixoY;
+        }
+        else if (instrucao == 'M' && orientacao == "O")
+        {
+            posicaoInicialEixoX = posicaoInicialEixoX + 1;
+            return posicaoInicialEixoX;
+        }
+        else if (instrucao == 'M' && orientacao == "L")
+        {
+            posicaoInicialEixoX = posicaoInicialEixoX - 1;
+            return posicaoInicialEixoX;
+        }
+    }
 }
-
-Console.WriteLine($"\nPosição Final: {posicaoInicialEixoX} {posicaoInicialEixoY} {orientacao}");
-Console.ReadLine();
-
-
